283624850dff40e02120bad2b0c5c27ebe228726
sergey gabrichidze <sergey.antonyukov@gmail.com>
2024-02-08 14:29:14 +0100

    corrected index.js

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("Your-LambdaFunction-URL");
+    let response = await fetch("https://2klzopq5rz3444gcis3bekmjsq0egsyy.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
