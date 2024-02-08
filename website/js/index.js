98639edf07e212a71de113d0bf5bae4c769e3ca6
sergey gabrichidze <sergey.antonyukov@gmail.com>
2024-02-08 13:11:57 +0100

    edited index.js

--- website/js/index.js
+++ website/js/index.js
@@ -1,8 +1,11 @@
 const counter = document.querySelector(".counter-number");
+
+// Function to update counter
 async function updateCounter() {
-    let response = await fetch("https://ebt2idsxwlt4lwy3y34g7far440wyhwc.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://2klzopq5rz3444gcis3bekmjsq0egsyy.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
 
-updateCounter();
+// Event listener for the link
+document.querySelector("images/Sergey_Antonyukov_CV_en.pdf").addEventListener("click", updateCounter);
