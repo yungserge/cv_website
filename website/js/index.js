73f0bb00c0d88dfec8cb5370734568a1427c2cbb
sergey gabrichidze <sergey.antonyukov@gmail.com>
2024-02-08 16:04:28 +0100

    no-cors mode edit

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,8 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://2klzopq5rz3444gcis3bekmjsq0egsyy.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://2klzopq5rz3444gcis3bekmjsq0egsyy.lambda-url.us-east-1.on.aws/", {
+        mode: 'no-cors'
+      });
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
