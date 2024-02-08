0cfda037df8331d6925fa6133760409133f9ec5d
sergey gabrichidze <sergey.antonyukov@gmail.com>
2024-02-08 17:03:23 +0100

    index test

--- website/js/index.js
+++ website/js/index.js
@@ -1,7 +1,10 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://2klzopq5rz3444gcis3bekmjsq0egsyy.lambda-url.us-east-1.on.aws/", {
-        mode: 'no-cors'
+        mode: 'cors',
+        headers: {
+            'Origin': 'https://yungserge.com' // Replace with your actual origin
+        }
       });
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
