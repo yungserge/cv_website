af5ede14edd7c7afb19dddece37a58b87ee842b1
sergey gabrichidze <sergey.antonyukov@gmail.com>
2024-02-08 14:18:12 +0100

    edited index.js

--- website/js/index.js
+++ website/js/index.js
@@ -1,11 +1,8 @@
 const counter = document.querySelector(".counter-number");
-
-// Function to update counter
 async function updateCounter() {
-    let response = await fetch("https://2klzopq5rz3444gcis3bekmjsq0egsyy.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("Your-LambdaFunction-URL");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
 
-// Event listener for the link
-document.querySelector("images/Sergey_Antonyukov_CV_en.pdf").addEventListener("click", updateCounter);
+updateCounter();
